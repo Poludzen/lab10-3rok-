62eef0bb024afc5cf3167fe001eea4c58f9a7f0a
Poludzen <80580379+Poludzen@users.noreply.github.com>
2022-02-01 15:44:50 +0300

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,13 +1,16 @@
 #include <iostream>
 
+// calculates d^2
 __device__ __host__  double f(double  d){
     return d * d;
 }
 
+//(for gpu) calculates integral from a to b with n points by trapezoid method, and saiving in to "out" variable
 __global__ void trapezoidIntegral(const double* a, const double* b, const int* n, double* out){
     const int ind = blockIdx.x * blockDim.x + threadIdx.x;
     const double width = (b[ind]-a[ind])/n[ind];
-
+    
+    // calculating integral
     double trapezoidal_integral = 0;
     for(int step = 0; step < n[ind]; step++) {
         const double x1 = a[ind] + step*width;
@@ -15,13 +18,16 @@ __global__ void trapezoidIntegral(const double* a, const double* b, const int* n
 
         trapezoidal_integral += 0.5*(x2-x1)*(f(x1) + f(x2));
     }
-
+    // saving
     out[ind] = trapezoidal_integral;
 }
+
+//(for gpu) calculates integral from a to b with n points by Simpson method, and saiving in to "out" variable
 __global__ void simpsonIntegral(const double* a, const double* b, const int* n, double* out){
     const int ind = blockIdx.x * blockDim.x + threadIdx.x;
     const double width = (b[ind]-a[ind])/n[ind];
-
+    
+    // calculating integral
     double simpson_integral = 0;
     for(int step = 0; step < n[ind]; step++) {
         const double x1 = a[ind] + step*width;
@@ -29,24 +35,28 @@ __global__ void simpsonIntegral(const double* a, const double* b, const int* n, 
 
         simpson_integral += (x2-x1)/6.0*(f(x1) + 4.0*f(0.5*(x1+x2)) + f(x2));
     }
-
+    // saving
     out[ind] = simpson_integral;
 }
+
+// (for gpu) calculates integral from a to b with n points by Rectangles method, and saiving in to "out" variable
 __global__ void pramougIntegral(const double* a, const double* b, const int* n, double* out){
     const int ind = blockIdx.x * blockDim.x + threadIdx.x;
     double x, h;
     double sum = 0.0;
     double fx;
-    h = (b[ind] - a[ind]) / n[ind];  //шаг
-
+    h = (b[ind] - a[ind]) / n[ind];  //step
+    // calculating integral
     for (int i = 0; i < n[ind]; i++) {
         x = a[ind] + i * h;
         fx = f(x);
         sum += fx;
     }
+    // saving
     out[ind] =  (sum * h);
 }
 
+//(for cpu) calculates integral from a to b with n points by trapezoid method, and returning result
 double trapezoidIntegral(const double a, const double b, const int n){
     const double width = (b - a) / n;
 
@@ -60,9 +70,11 @@ double trapezoidIntegral(const double a, const double b, const int n){
 
     return trapezoidal_integral;
 }
+
+//(for cpu) calculates integral from a to b with n points by Simpson method, and returning result
 double simpsonIntegral(const double a, const double b, const int n){
     const double width = (b-a)/n;
-
+    // calculating integral
     double simpson_integral = 0;
     for(int step = 0; step < n; step++) {
         const double x1 = a + step*width;
@@ -70,24 +82,28 @@ double simpsonIntegral(const double a, const double b, const int n){
 
         simpson_integral += (x2-x1)/6.0*(f(x1) + 4.0*f(0.5*(x1+x2)) + f(x2));
     }
-
+    // returning result
     return simpson_integral;
 }
+
+//(for cpu) calculates integral from a to b with n points by Rectangles method, and returning result
 double pramougIntegral(const double a, const double b, const int n){
     double x, h;
     double sum = 0.0;
     double fx;
-    h = (b - a) / n;  //шаг
-
+    h = (b - a) / n;  //step
+    // calculating integral
     for (int i = 0; i < n; i++) {
         x = a + i * h;
         fx = f(x);
         sum += fx;
     }
+    // returning result
     return sum * h;
 }
 
-
+// testing calculation with Rectangles method
+// for CPU:
 double* IntegralTestCPU(int count, double a_, double b_, int n_){
     auto* arr = (double *)malloc(sizeof(double) * count);
     for (int i = 0; i < count; i++){
@@ -95,6 +111,7 @@ double* IntegralTestCPU(int count, double a_, double b_, int n_){
     }
     return arr;
 }
+// and for GPU:
 double* IntegralTestGPU(int count, double a_, double b_, int n_){
     double *a, *b, *out;
     int *n;
@@ -126,6 +143,8 @@ double* IntegralTestGPU(int count, double a_, double b_, int n_){
 
 
 int main() {
+    // starting experiment
+    // test time on CPU
     time_t  start = clock();
     double* a = IntegralTestCPU(100000, 1, 1000, 100000);
     time_t end = clock();
@@ -133,7 +152,7 @@ int main() {
     printf("The time: %f seconds\n", seconds);
 
 
-
+    // test time on GPU
     time_t  start1 = clock();
     double* a1 = IntegralTestGPU(100000, 1, 1000, 100000);
     time_t end1 = clock();
